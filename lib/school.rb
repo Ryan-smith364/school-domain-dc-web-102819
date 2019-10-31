0b721163703b0349c68e34723e9bed0deb318ecd
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-31 14:22:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_accessor :name
+  def initialize (name)
+    @name = name
+   
+    @roster ={}
+  end 
+  
+  def add_student (name, grade)
+    
+  end
+  
+  def grade(grade)
+    
+  end
+  
+  def sort
+    
+  end
+  
+end 
